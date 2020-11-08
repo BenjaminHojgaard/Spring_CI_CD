fb8e6875b100218469c17612db742a77f9c45828
Benjamin Højgaard <benjaminoest@hotmail.com>
2020-11-08 15:04:04 +0100

    failing test

---
+++ src/test/java/com/example/demo/unit/DemoTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.unit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DemoTest {
+
+
+    @Test
+    void addition() {
+        assertEquals(1, 2);
+    }
+
+}
