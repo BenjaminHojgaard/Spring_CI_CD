30228154dda7aa89249b3a99f176c640793ea014
Benjamin Højgaard <benjaminoest@hotmail.com>
2020-11-12 21:57:14 +0100

    using RoomDTO from Github Package Registry

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import dto.HotelDTO;
+import dto.RoomDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import types.RoomType;
 
 @SpringBootApplication
 @Controller
@@ -12,8 +15,9 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
+        RoomDTO room = new RoomDTO(new HotelDTO(), "22", RoomType.F);
         model.addAttribute("title", "Docker + Spring Boot");
-        model.addAttribute("msg", "Welcome to the LSD Zone!");
+        model.addAttribute("msg", "This is the RoomDTO properties: " + room.getRoomNumber() + " " + room.getRoomType().toString());
         return "index";
     }
 
