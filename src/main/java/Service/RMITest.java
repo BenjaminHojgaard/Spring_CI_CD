0004c05e73b31f715f9aed692581db418f75e28e
Benjamin Højgaard <benjaminoest@hotmail.com>
2020-11-24 14:37:17 +0100

    added RMI

--- src/main/java/Service/RMITest.java
+++ src/main/java/Service/RMITest.java
@@ -1,8 +1,41 @@
 package Service;
 
-import java.rmi.Naming;
+import dto.HotelDTO;
+import dto.HotelDetailsDTO;
+import org.springframework.stereotype.Service;
+import service.HotelUtility;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+@Service
 public class RMITest {
 
-    String backendURL = "";
+    String backendURL = "rmi://localhost/HotelService";
+    String AWS = "rmi://ec2-18-223-161-64.us-east-2.compute.amazonaws.com/hotelserver";
+    HotelUtility obj;
+
+    public RMITest() {
+        try {
+            obj = (HotelUtility) Naming.lookup(AWS);
+        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getHotels() throws RemoteException {
+        var hotelsToReturn = (ArrayList<HotelDTO>)obj.fetchHotels("Berlin", new Date(2020, Calendar.DECEMBER, 24), 1);
+        for (HotelDTO dto: hotelsToReturn) {
+            System.out.println(dto.getName());
+        }
+    }
+
+    public boolean createBooking() throws RemoteException {
+        return obj.createBooking("","");
+    }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import Service.RMITest;
 import dto.RoomDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import types.RoomType;
 
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
 @SpringBootApplication
 @Controller
 public class DemoApplication {
 
     @GetMapping("/")
-    public String index(final Model model) {
+    public String index(final Model model) throws RemoteException, MalformedURLException {
         RoomDTO room = new RoomDTO(null, "24", RoomType.F);
         model.addAttribute("title", "Docker + Spring Boot");
         model.addAttribute("msg", "This is the RoomDTO properties: " + room.getRoomNumber() + room.getRoomType());
+
+        var RMI = new RMITest();
+        System.out.println(RMI.createBooking());
         return "index";
     }
 
