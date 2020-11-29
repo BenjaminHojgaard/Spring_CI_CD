8b3b7b969a440e6a079aab5489f8d5f805572733
Malene <malene1105@gmail.com>
2020-11-29 12:07:55 +0100

    tests

--- src/test/java/com/example/demo/unit/HotelServiceTest.java
+++ src/test/java/com/example/demo/unit/HotelServiceTest.java
@@ -1,18 +1,42 @@
 package com.example.demo.unit;
 
+import com.example.demo.Service.HotelService;
+import dto.BookingDTO;
+import dto.RoomBookingDTO;
+import dto.RoomDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
 public class HotelServiceTest {
-    
-    
-    @Test
-    public void mustWhen() {
+
+    @Mock RoomBookingDTO roomBookingMock;
+    @Mock RoomDTO roomMock;
+
+
+  /*  @Test
+    public void mustReturnTrueWhenABookingIsMade() {
         //Arrange
-        var expected = ;
-        
+        ArrayList<RoomBookingDTO> roomBookings =  new ArrayList<>();
+        roomBookings.add(roomBookingMock);
+
+        BookingDTO booking = new BookingDTO("passportnumber", roomBookings, false, 3 {
+
         //Act
-        var result = ;
+
+        HotelService hotelService = new HotelService();
+
+
+            var result = ;
         
         //Assert
         assertEquals(expected, result);
     
-    }
+    }*/
 }
