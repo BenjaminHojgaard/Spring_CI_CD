77f365f1945d49245df3fec8a10aa4a3e6bb1ede
Malene <malene1105@gmail.com>
2020-11-29 11:26:49 +0100

    HotelServiceTest created

---
+++ src/test/java/com/example/demo/unit/HotelServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.unit;
+
+public class HotelServiceTest {
+    
+    
+    @Test
+    public void mustWhen() {
+        //Arrange
+        var expected = ;
+        
+        //Act
+        var result = ;
+        
+        //Assert
+        assertEquals(expected, result);
+    
+    }
+}
