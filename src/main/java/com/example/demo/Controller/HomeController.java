9de1b4a30aab6105133642e22e32ccfcb4a43d49
Highyard <benjaminoest@hotmail.com>
2020-11-30 22:48:42 +0100

    banner update

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Docker + Spring Boot");
-        model.addAttribute("msg", "We used to do RMI, but now we're using REST!");
+        model.addAttribute("msg", "If you are reading this, then the frontend works!");
         logger.info("index.html successfully loaded");
         return "index";
     }
